f14e8f0b5bae511e4e835aaea354845208a244ae
hurbx <hugo.urbina@mayor.cl>
2023-10-24 18:26:34 -0300

    commit 5

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # table-admin-backend
-Backend para administración mesas de un restaurant 
+## Proyecto en desarrollo...
+
+El proyecto tiene como objetivo crear una aplicacion
+web para la administracion de las mesas de un restaurant,
+su consumo total, productos agregados a las mesas,
+trabajador encargado de la mesa, etc, luego ser
+capaces de generar una boleta, con el monto total,
+propina y detalle de los productos consumidos. 
+## Tecnologias
+- Python
+- Django
+- Django Rest Framework
+- PostgreSQL (en un futuro)
+- Cloud AWS
+
+---
+MER
+---
+```mermaid
+  erDiagram
+      Mesa ||--|{ Productos : has
+      Mesa ||--|{ Empleado : has
+      Mesa ||--|{ Boleta : has
+      Boleta ||--|| Poprina: has
+      Empleado ||--|| Tipo : has
+      Productos ||--|{ Categoria : has
+      Categoria ||--|| Descuento : has
+      Boleta ||--|{ Empleado : has
+      Boleta ||--|| Mesa : has
+      Boleta ||--|{ Productos : has
+      
+```
+## Project in development...
+
+The project aims to create an application
+website for the administration of tables in a restaurant,
+your total consumption, products added to the tables,
+worker in charge of the table, etc., then be
+capable of generating a ticket, with the total amount,
+tip and details of the products consumed.
+## Technologies
+-Python
+-Django
+- Django Rest Framework
+- PostgreSQL (in the future)
+- AWS Cloud
+
+---
+MER
+---
+```mermaid
+  erDiagram
+      Table ||--|{ Products : has
+      Table ||--|{ Employee : has
+      Table ||--|{ Ticket : has
+      Ticket ||--|| Tip: has
+      Employee ||--|| Tipe : has
+      Products ||--|{ Category : has
+      Category ||--|| Descuento : has
+      Ticket ||--|{ Employee : has
+      Ticket ||--|| Table : has
+      Ticket ||--|{ Products : has
+      
+```
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+class Tip(models.Model):
+    tip_percentage = models.IntegerField('Percentage', blank=False, null=False)
+
+    tip = models.Manager()
+
+    def __str__(self):
+        return f'{self.id} - {self.tip_percentage}'
+
+
 class Discount(models.Model):
     discount_percentage = models.IntegerField('Percentage', blank=False, null=False)
 
@@ -13,6 +22,8 @@ class Discount(models.Model):
         verbose_name = 'Discount'
         verbose_name_plural = 'Discounts'
 
+
+#    bus = models.ForeignKey(Bus, on_delete=models.CASCADE, null=True, blank=True)
 
 class Type(models.Model):
     type_name = models.CharField('Name', max_length=50, blank=False, null=False)
@@ -27,12 +38,13 @@ class Type(models.Model):
         verbose_name_plural = 'Types'
 
 
-class User(models.Model):
-    user_name = models.CharField('Name', max_length=50, blank=False, null=False)
-    user_password = models.CharField('Password', max_length=50, blank=False, null=False)
-    user_email = models.CharField('Email', max_length=50, blank=False, null=False)
+class Employee(models.Model):
+    employee_name = models.CharField('Name', max_length=50, blank=False, null=False)
+    employee_password = models.CharField('Password', max_length=50, blank=False, null=False)
+    employee_email = models.CharField('Email', max_length=50, blank=False, null=False)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE, null=True, blank=True)
 
-    user = models.Manager()
+    employee = models.Manager()
 
     def __str__(self):
         return f'{self.id} - {self.user_name} - {self.user_email} - {self.user_password}'
@@ -61,6 +73,7 @@ class Table(models.Model):
     table_name = models.CharField('Name', max_length=50, blank=False, null=False)
     creation_date = models.DateTimeField('Creation Date', auto_now_add=True)
     payment_date = models.DateTimeField('Payment Date', blank=True, null=True)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, null=True, blank=True)
 
     table = models.Manager()
 
@@ -74,7 +87,9 @@ class Table(models.Model):
 
 class Ticket(models.Model):
     total = models.IntegerField('Total', blank=False, null=False)
-    tips = models.IntegerField('Tips', blank=False, null=False)
+    tips = models.ForeignKey(Tip, on_delete=models.CASCADE, null=True, blank=True)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE, null=True, blank=True)
+    discount = models.ForeignKey(Discount, on_delete=models.CASCADE, null=True, blank=True)
 
     ticket = models.Manager()
 
